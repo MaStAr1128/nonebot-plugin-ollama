a8b3ca5ea71a440324c5f24df31565ff91c4253a
MaStAr <2024381053@mails.szu.edu.cn>
2024-12-16 22:12:56 +0800

    Update config.py
    建议把群聊和私聊区分开来

--- src/nonebot_plugin_ollama/config.py
+++ src/nonebot_plugin_ollama/config.py
@@ -7,20 +7,6 @@ class ScopedConfig(BaseModel):
     max_histories: int = 50
     listening: str = []
     cmd: str = []
-
-    @field_validator('min_priority')
-    @classmethod
-    def check_priority(cls, v: int):
-        if v >= 1:
-            return v
-        raise ValueError('ollama command priority must be greater than 1')
-    
-    @field_validator('url')
-    @classmethod
-    def check_url(cls, v: str):
-        if v.startswith('http://') and v.endswith('/'):
-            return v
-        raise ValueError('ollama url must start with http:// and end with /')
     
 class Config(BaseModel):
     ollama: ScopedConfig = ScopedConfig()
